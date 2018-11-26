5321fa156e94b7fb6e99a170ef79101b2ab8b4b6
dpope5452 <dpope@dstsystems.com>
2018-11-26 11:15:11 -0600

    added more search functionality, added comments, and some error handling

--- FinalProject/BasketballStatsForm.cs
+++ FinalProject/BasketballStatsForm.cs
@@ -28,35 +28,46 @@ namespace FinalProject
 			return char.ToUpper(s[0]) + s.Substring(1);
 		}
 
+		//adds a player to the database
 		private void addPlayerButton_Click(object sender, EventArgs e)
         {
+			addPlayerConsole.Text = null;
             SQLiteConnection connection = new SQLiteConnection(@"Data Source=.\basketball.sqlite3");
             var basketballDB = new BasketballDB(connection);
 			var players = basketballDB.GetTable<Player>();
 
+			//gets user inputted player information
             var name = playerNameTextBox.Text;
             var number = numberUpDown.Value;
 			var position = "";
-            foreach (RadioButton rdo in positionBox.Controls.OfType<RadioButton>())
-            {
-                if (rdo.Checked)
-                {
-                    position = rdo.Text;
-                }
-            }
+			int j;
+			foreach (RadioButton rdo in positionBox.Controls.OfType<RadioButton>())
+			{
+				if (rdo.Checked)
+				{
+					position = rdo.Text;
+				}
+			}
 			var team = teamListBox.GetItemText(teamListBox.SelectedItem);
+			if (int.TryParse(name, out j))
+			{
+				addPlayerConsole.Text = "Name cannot be an integer.";
+			}
+			else
+			{
+				var player = new Player(name, number, team, position);
 
-			var player = new Player(name, number, team, position);
+				//inserts new player into the database
+				players.InsertOnSubmit(player);
+				basketballDB.SubmitChanges();
 
-			players.InsertOnSubmit(player);
-			basketballDB.SubmitChanges();
-
-			addPlayerConsole.Text = $"Successfully added {name} to the {team}";
-
+				addPlayerConsole.Text = $"Successfully added {name} to the {team}";
+			}
 			basketballDB.Dispose();
             connection.Close();
         }
 
+		//gets a list of the teams in the database
         private void refreshTeamsButton_Click(object sender, EventArgs e)
         {
             teamListBox.Items.Clear();
@@ -71,6 +82,7 @@ namespace FinalProject
             connection.Close();
         }
 
+		//resets the form fields in add player tab
         private void addPlayerReset_Click(object sender, EventArgs e)
         {
 			foreach (Control control in StatsTab.SelectedTab.Controls)
@@ -105,6 +117,7 @@ namespace FinalProject
 			addPlayerConsole.Text = null;
 		}
 
+		//loads the teams in the add game tab
 		private void addGameLoadTeams_Click(object sender, EventArgs e)
 		{
 			teamOneListBox.Items.Clear();
@@ -121,6 +134,7 @@ namespace FinalProject
 			connection.Close();
 		}
 
+		//adds a game to the database
 		private void submitScoreButton_Click(object sender, EventArgs e)
 		{
             addGameConsole.Text = null;
@@ -137,63 +151,72 @@ namespace FinalProject
 			var teamTwo = teamTwoListBox.GetItemText(teamTwoListBox.SelectedItem);
 			var teamOnePoints = int.Parse(teamOnePointsTextBox.Text);
 			var teamTwoPoints = int.Parse(teamTwoPointsTextBox.Text);
+			int j;
 
-			var game = new Game(id, teamOne, teamTwo, teamOnePoints, teamTwoPoints);
-
-			games.InsertOnSubmit(game);
-			basketballDB.SubmitChanges();
-            //ask Mark about this
-            String consoleText = $"{teamOne} VS {teamTwo}";
-            addGameConsole.Text = consoleText;
-
-			if (teamOnePoints > teamTwoPoints)
+			if (teamOne == teamTwo)
 			{
-				var winningTeam =
-					from team in basketballDB.Teams
-					where team.Name == teamOne
-					select team;
-                var losingTeam =
-                    from team in basketballDB.Teams
-                    where team.Name == teamTwo
-                    select team;
-				foreach (var team in winningTeam)
-				{
-					team.Wins++;
-				}
-                foreach (var team in losingTeam)
-                {
-                    team.Losses++;
-                }
-				basketballDB.SubmitChanges();
-                addGameConsole.Text += $"\n{teamOne} win!";
+				addGameConsole.Text = "Team One cannot be the same as team two";
 			}
 			else
 			{
-				var winningTeam =
-					from team in basketballDB.Teams
-					where team.Name == teamTwo
-					select team;
-                var losingTeam =
-                    from team in basketballDB.Teams
-                    where team.Name == teamOne
-                    select team;
-                foreach (var team in winningTeam)
-				{
-					team.Wins++;
-				}
-                foreach (var team in losingTeam)
-                {
-                    team.Losses++;
-                }
+				var game = new Game(id, teamOne, teamTwo, teamOnePoints, teamTwoPoints);
+				//inserts new game into database
+				games.InsertOnSubmit(game);
 				basketballDB.SubmitChanges();
-                addGameConsole.Text += $"\n{teamTwo} get the win!";
-			}
+				//ask Mark about this
+				String consoleText = $"{teamOne} VS {teamTwo}";
+				addGameConsole.Text = consoleText;
 
+				//determines who won and lost the game and adds a win and loss to each respectively
+				if (teamOnePoints > teamTwoPoints)
+				{
+					var winningTeam =
+						from team in basketballDB.Teams
+						where team.Name == teamOne
+						select team;
+					var losingTeam =
+						from team in basketballDB.Teams
+						where team.Name == teamTwo
+						select team;
+					foreach (var team in winningTeam)
+					{
+						team.Wins++;
+					}
+					foreach (var team in losingTeam)
+					{
+						team.Losses++;
+					}
+					basketballDB.SubmitChanges();
+					addGameConsole.Text += $"\n{teamOne} win!";
+				}
+				else
+				{
+					var winningTeam =
+						from team in basketballDB.Teams
+						where team.Name == teamTwo
+						select team;
+					var losingTeam =
+						from team in basketballDB.Teams
+						where team.Name == teamOne
+						select team;
+					foreach (var team in winningTeam)
+					{
+						team.Wins++;
+					}
+					foreach (var team in losingTeam)
+					{
+						team.Losses++;
+					}
+					basketballDB.SubmitChanges();
+					addGameConsole.Text += $"\n{teamTwo} get the win!";
+				}
+			}
 
 			basketballDB.Dispose();
 			connection.Close();
 		}
 
+		//resets the form fields in the add game tab
         private void resetAddGame_Click(object sender, EventArgs e)
         {
             foreach (Control control in StatsTab.SelectedTab.Controls)
@@ -213,6 +236,7 @@ namespace FinalProject
             addGameConsole.Text = null;
         }
 
+		//adds a team to the database
         private void addTeamButton_Click(object sender, EventArgs e)
         {
             addGameConsole.Text = null;
@@ -236,6 +260,7 @@ namespace FinalProject
 			connection.Close();
 		}
 
+		//resets the form fields in the add team tab
         private void addTeamReset_Click(object sender, EventArgs e)
         {
             foreach (Control control in StatsTab.SelectedTab.Controls)
@@ -250,6 +275,7 @@ namespace FinalProject
             addTeamConsole.Text = null;
         }
 
+		//checks to see if name is checked in player search
 		private void nameCheckBox_CheckedChanged(object sender, EventArgs e)
 		{
 			if (playerNameSearch.Enabled == true)
@@ -262,6 +288,7 @@ namespace FinalProject
 			}
 		}
 
+		//checks to see if number box is checked in player search
 		private void numberCheckBox_CheckedChanged(object sender, EventArgs e)
 		{
 			if (playerNumberSearch.Enabled == true)
@@ -274,6 +301,7 @@ namespace FinalProject
 			}
 		}
 
+		//checks to see if team box is checked in player search
 		private void teamCheckBox_CheckedChanged(object sender, EventArgs e)
 		{
 			if (playerTeamSearch.Enabled == true)
@@ -286,6 +314,7 @@ namespace FinalProject
 			}
 		}
 
+		//checks to see if position box is checked in player search
 		private void positionCheckBox_CheckedChanged(object sender, EventArgs e)
 		{
 			if (playerPositionSearch.Enabled == true)
@@ -298,6 +327,7 @@ namespace FinalProject
 			}
 		}
 
+		//checks to see if only one team is going to be searched in game search
 		private void oneTeamSearchButton_CheckedChanged(object sender, EventArgs e)
 		{
 			if (teamOneGameSearch.Enabled == false)
@@ -310,6 +340,7 @@ namespace FinalProject
 			}
 		}
 
+		//checks to see if two teams are going to be searched in game search
 		private void twoTeamsSearchButton_CheckedChanged(object sender, EventArgs e)
 		{
 			if (teamOneGameSearch.Enabled == false)
@@ -322,6 +353,7 @@ namespace FinalProject
 			}
 		}
 
+		//checks to see if name box is checked in team search
 		private void teamNameCheck_CheckedChanged(object sender, EventArgs e)
 		{
 			if (teamNameSearch.Enabled == true)
@@ -334,6 +366,7 @@ namespace FinalProject
 			}
 		}
 
+		//checks to see if city box is checked in team search
 		private void teamCityCheck_CheckedChanged(object sender, EventArgs e)
 		{
 			if (teamCitySearch.Enabled == true)
@@ -346,6 +379,7 @@ namespace FinalProject
 			}
 		}
 
+		//searches by player
 		private void searchPlayers_Click(object sender, EventArgs e)
 		{
 			playerSearchConsole.Text = null;
@@ -357,6 +391,7 @@ namespace FinalProject
 			var team = "";
 			var position = "";
 
+			//checks to see what the user is searching by
 			if (playerNameSearch.Enabled == true)
 			{
 				name = playerNameSearch.Text;
@@ -378,91 +413,149 @@ namespace FinalProject
 				playerSearchConsole.Text = "You can't search nothing, duh.";
 			}
 
+			IQueryable<Player> searchedPlayers = null;
+
+			//searches by player name
 			if (name != "" && number == -1 && team == "" && position == "")
 			{
-				var searchedPlayers =
+				searchedPlayers =
 					from player in basketballDB.Players
 					where player.Name == name
 					select player;
-				playerSearchConsole.Text = "Name\t\tNumber\tTeam\tPosition";
-				playerSearchConsole.Text += "\n-----------------------------------------------------------------------------------";
-				if (!searchedPlayers.Any())
-				{
-					playerSearchConsole.Text = "Nothing found.";
-				}
-				else
-				{
-					foreach (var player in searchedPlayers)
-					{
-						playerSearchConsole.Text += $"\n{player.Name}\t{player.Number}\t{player.Team}\t{player.Position}";
-					}
-				}
 			}
+			//searches by player number
 			else if (name == "" && number != -1 && team == "" && position == "")
 			{
-				var searchedPlayers =
+				searchedPlayers =
 					from player in basketballDB.Players
 					where player.Number == number
 					select player;
-				playerSearchConsole.Text = "Name\t\tNumber\tTeam\tPosition";
-				playerSearchConsole.Text += "\n-----------------------------------------------------------------------------------";
-				if (!searchedPlayers.Any())
-				{
-					playerSearchConsole.Text = "Nothing found.";
-				}
-				else
-				{
-					foreach (var player in searchedPlayers)
-					{
-						playerSearchConsole.Text += $"\n{player.Name}\t{player.Number}\t{player.Team}\t{player.Position}";
-					}
-				}
 			}
+			//searches by player's team
 			else if (name == "" && number == -1 && team != "" && position == "")
 			{
-				var searchedPlayers =
+				searchedPlayers =
 					from player in basketballDB.Players
 					where player.Team == team
 					select player;
-				playerSearchConsole.Text = "Name\t\tNumber\tTeam\tPosition";
-				playerSearchConsole.Text += "\n-----------------------------------------------------------------------------------";
-				if (!searchedPlayers.Any())
-				{
-					playerSearchConsole.Text = "Nothing found.";
-				}
-				else
-				{
-					foreach (var player in searchedPlayers)
-					{
-						playerSearchConsole.Text += $"\n{player.Name}\t{player.Number}\t{player.Team}\t{player.Position}";
-					}
-				}
 			}
+			//searches by player's position
 			else if (name == "" && number == -1 && team == "" && position != "")
 			{
-				var searchedPlayers =
+				searchedPlayers =
 					from player in basketballDB.Players
 					where player.Position == position
 					select player;
-				playerSearchConsole.Text = "Name\t\tNumber\tTeam\tPosition";
-				playerSearchConsole.Text += "\n-----------------------------------------------------------------------------------";
-				if (!searchedPlayers.Any())
-				{
-					playerSearchConsole.Text = "Nothing found.";
-				}
-				else
-				{
-					foreach (var player in searchedPlayers)
-					{
-						playerSearchConsole.Text += $"\n{player.Name}\t{player.Number}\t{player.Team}\t{player.Position}";
-					}
-				}
+			}
+			//searcbes by player name and number
+			else if (name != "" && number != -1 && team == "" && position != "")
+			{
+				searchedPlayers =
+					from player in basketballDB.Players
+					where player.Name == name && player.Number == number
+					select player;
+			}
+			//searches by player name and team
+			else if (name != "" && number == -1 && team != "" && position == "")
+			{
+				searchedPlayers =
+					from player in basketballDB.Players
+					where player.Name == name && player.Team == team
+					select player;
+			}
+			//searches by player name and position
+			else if (name != "" && number == -1 && team == "" && position != "")
+			{
+				searchedPlayers =
+					from player in basketballDB.Players
+					where player.Name == name && player.Position == position
+					select player;
+			}
+			//searches by player number and team
+			else if (name == "" && number != -1 && team != "" && position == "")
+			{
+				searchedPlayers =
+					from player in basketballDB.Players
+					where player.Number == number && player.Team == team
+					select player;
+			}
+			//searches by player number and position
+			else if (name == "" && number != -1 && team == "" && position != "")
+			{
+				searchedPlayers =
+					from player in basketballDB.Players
+					where player.Number == number && player.Position == position
+					select player;
+			}
+			//searches by player team and position
+			else if (name == "" && number == -1 && team != "" && position != "")
+			{
+				searchedPlayers =
+					from player in basketballDB.Players
+					where player.Team == team && player.Position == position
+					select player;
+			}
+			//searches by player name, number, and team
+			else if (name != "" && number != -1 && team != "" && position == "")
+			{
+				searchedPlayers =
+					from player in basketballDB.Players
+					where player.Name == name && player.Number == number && player.Team == team
+					select player;
+			}
+			//searches by player name, number, and position
+			else if (name != "" && number != -1 && team == "" && position != "")
+			{
+				searchedPlayers =
+					from player in basketballDB.Players
+					where player.Name == name && player.Number == number && player.Position == position
+					select player;
+			}
+			//searches by player name, team, and position
+			else if (name != "" && number == -1 && team != "" && position != "")
+			{
+				searchedPlayers =
+					from player in basketballDB.Players
+					where player.Name == name && player.Team == team && player.Position == position
+					select player;
+			}
+			//searches by player number, team, and position
+			else if (name == "" && number != -1 && team != "" && position != "")
+			{
+				searchedPlayers =
+					from player in basketballDB.Players
+					where player.Number == number && player.Team == team && player.Position == position
+					select player;
+			}
+			//searches by name, number, team, position
+			else if (name != "" && number != -1 && team != "" && position != "")
+			{
+				searchedPlayers =
+					from player in basketballDB.Players
+					where player.Name == name && player.Number == number && player.Team == team && player.Position == position
+					select player;
 			}
 
+
+			//outputs onto console
+			playerSearchConsole.Text = "Name\t\tNumber\tTeam\tPosition";
+			playerSearchConsole.Text += "\n-----------------------------------------------------------------------------------";
+			if (!searchedPlayers.Any())
+			{
+				playerSearchConsole.Text = "Nothing found.";
+			}
+			else
+			{
+				foreach (var player in searchedPlayers)
+				{
+					playerSearchConsole.Text += $"\n{player.Name}\t{player.Number}\t{player.Team}\t{player.Position}";
+				}
+			}
 			basketballDB.Dispose();
 			connection.Close();
 		}
 
+		//searches by game
 		private void searchGames_Click(object sender, EventArgs e)
 		{
 			SQLiteConnection connection = new SQLiteConnection(@"Data Source=.\basketball.sqlite3");
@@ -473,6 +566,7 @@ namespace FinalProject
 			connection.Close();
 		}
 
+		//searches by team
 		private void searchTeams_Click(object sender, EventArgs e)
 		{
 			SQLiteConnection connection = new SQLiteConnection(@"Data Source=.\basketball.sqlite3");
